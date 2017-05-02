eeea4fcc9806805a943b0a5dcf4e00201dcf4b88
theorak <p.roensch@datec-schmidt.de>
2017-05-02 16:10:40 +0200

    Version 1.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,40 +1,31 @@
 <?php
 
-/***************************************************************
+/*********************************************************************
  * Extension Manager/Repository config file for ext "datec_losungen".
- *
- * Auto generated 23-02-2016 12:12
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
+ *********************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'Datec Losungen',
 	'description' => 'Adds a plugin to display the daily german watchwords (Losungen).',
 	'category' => 'plugin',
 	'author' => 'Philipp Roensch',
 	'author_email' => 'p.roensch@datec-schmidt.de',
 	'author_company' => 'Datentechnik Schmidt Software GmbH',
-	'state' => 'stable',
-	'uploadfolder' => true,
 	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
 	'version' => '1.2.0',
-	'constraints' => 
-	array (
-		'depends' => 
-		array (
-			'typo3' => '7.6.0-8.99.99',
-		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
-	'clearcacheonload' => false,
-);
-
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.99.99',
+        ],
+        'conflicts' => [],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Datec\\DatecLosungen\\' => 'Classes'
+        ],
+    ],	
+    'state' => 'stable',
+    'uploadfolder' => 1,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+];
